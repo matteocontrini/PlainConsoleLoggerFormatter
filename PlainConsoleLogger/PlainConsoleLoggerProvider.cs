48e0e110750fef00dc134c1e538b39b83d13ee1d
Matteo Contrini <matteo@contrini.it>
2019-09-15 14:54:23 +0200

    Initial version

---
+++ PlainConsoleLogger/PlainConsoleLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+
+namespace PlainConsoleLogger
+{
+    public class PlainConsoleLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, ILogger> loggers;
+        private readonly PlainConsoleLoggerQueue messageQueue;
+
+        public PlainConsoleLoggerProvider()
+        {
+            this.loggers = new ConcurrentDictionary<string, ILogger>();
+            this.messageQueue = new PlainConsoleLoggerQueue();
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return this.loggers.GetOrAdd(categoryName, c => new PlainConsoleLogger(c, this.messageQueue));
+        }
+
+        public void Dispose()
+        {
+            this.messageQueue.Dispose();
+        }
+    }
+}
---
+++ PlainConsoleLogger/PlainConsoleLoggerQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace PlainConsoleLogger
+{
+    internal class PlainConsoleLoggerQueue : IDisposable
+    {
+        private readonly Thread thread;
+        private readonly BlockingCollection<string> collection;
+        private const int QUEUE_CAPACITY = 1024;
+
+        public PlainConsoleLoggerQueue()
+        {
+            this.collection = new BlockingCollection<string>(QUEUE_CAPACITY);
+
+            this.thread = new Thread(ProcessQueue)
+            {
+                IsBackground = true,
+                Name = "Console logger queue processing thread"
+            };
+
+            this.thread.Start();
+        }
+
+        public void EnqueueMessage(string message)
+        {
+            try
+            {
+                this.collection.Add(message);
+            }
+            // Object is disposed or adding is complete
+            catch
+            {
+                WriteMessage(message);
+            }
+        }
+
+        private void WriteMessage(string message)
+        {
+            Console.Write(message);
+        }
+
+        private void ProcessQueue()
+        {
+            foreach (var message in this.collection.GetConsumingEnumerable())
+            {
+                WriteMessage(message);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.collection.CompleteAdding();
+
+            try
+            {
+                this.thread.Join(TimeSpan.FromMilliseconds(1500));
+            }
+            catch (ThreadStateException)
+            {
+            }
+        }
+    }
+}
